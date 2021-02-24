5433d19cf51b82cec984f3e09a0fba51cce166c5
unknown <you@example.com>
2021-02-24 09:54:54 +0530

    jdk 8 setup

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
-@RequestMapping("/hm")
+@RequestMapping("/")
 public String handler(){
 	return "home";
 }
